1394b715a626e618b31a6777844c6082322dd0bb
Giovanna Pelati <giovanna@pelati.com.br>
2025-09-29 14:56:40 -0300

    refactor: alteração do modelo do guardrail

--- agents/guardrail_agent.py
+++ agents/guardrail_agent.py
@@ -23,7 +23,7 @@ class GuardrailOutput(BaseModel):
     
 # Conecta com o Gemini para geração de respostas
 model = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+    model="gemini-2.0-flash",
     google_api_key=os.getenv("GEMINI_API_KEY"),
 ).with_structured_output(GuardrailOutput)
 
